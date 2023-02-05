64b39e322d607eacc737ca82f0b07d49b585101e
Alysson Lopes <nexus.af@gmail.com>
2023-02-05 00:36:36 -0400

    Empty database

--- db/users.js
+++ db/users.js
@@ -1,22 +1,2 @@
 export default [
-	{
-		"userId": "AAAA1",
-		"nickName": "Aly",
-		"register_date": "2023-02-04T22:41:20.360Z"
-	},
-	{
-		"userId": "AAAFA",
-		"nickName": "Aly",
-		"register_date": "2023-02-04T22:43:38.222Z"
-	},
-	{
-		"userId": "TEST1",
-		"nickName": "Aly",
-		"register_date": "2023-02-05T03:17:34.415Z"
-	},
-	{
-		"userId": "ABCDE",
-		"nickName": "",
-		"register_date": "2023-02-05T04:18:00.148Z"
-	}
 ];
